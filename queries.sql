70e587edc0222900c56278078078c874b3954dc1
Kaya-F <kfisher@appdev.yearup.org>
2023-06-09 14:44:46 -0400

    Dealership Values

--- queries.sql
+++ queries.sql
@@ -1,1 +1,3 @@
-
+INSERT INTO Dealerships VALUES(1, 'ABC Motors', '123 Main St', '555-123-4567');
+INSERT INTO Dealerships VALUES(2, 'XYZ Auto', '456 Elm Ave', '555-987-6543');
+INSERT INTO Dealerships VALUES(3, 'Sunshine Cars', '789 Oak Blvd', '555-789-1234');
